a97b45eea1cfc4b2975e379409c7fc80aea85d99
SFNozomi <35213529+SaltyChiang@users.noreply.github.com>
2021-04-05 17:28:50 +0800

    Use a single file instead of a directory. Also improved the type information of functions.

--- vs_plugins_helper.py
+++ vs_plugins_helper.py
@@ -1,9 +1,8 @@
 import os
 import sys
 import re
+from typing import Dict, List, Union
 from vapoursynth import core
-
-plugins = core.get_plugins()
 
 vs_path = "."
 init_in_path = "vs_plugins_helper.in"
@@ -11,23 +10,37 @@ init_in_path = "vs_plugins_helper.in"
 if len(sys.argv) > 1 and sys.argv[1] is not None:
     vs_path = sys.argv[1]
     if not os.path.exists(vs_path):
-        print("Path \"%s\" invalid." % (vs_path))
+        print(f'Path "{vs_path}" invalid.')
         sys.exit()
 
-vs_path = "%s/vapoursynth" % (vs_path)
+vs_path = f"{vs_path}/vapoursynth.pyi"
 if os.path.exists(vs_path):
-    print(
-        "There exists a folder called \"vapoursynth\" in the path, do you want to cover it? (y/N)"
-    )
+    print('There exists "vapoursynth.pyi" in the path, do you want to cover it? (y/N)')
     cover = input().lower()
     if cover != "y" and cover != "yes":
         print("Stop generating.")
         sys.exit()
-else:
-    os.mkdir(vs_path)
 
 
-def convert2pyi(params: str):
+class FunctionMeta(Dict[str, Union[str, None]]):
+    pass
+
+
+class PluginMeta:
+    namespace: str
+    name: str
+    functions_core: FunctionMeta
+    functions_video: FunctionMeta
+
+    def __init__(self, namespace: str, name: str, functions: List[FunctionMeta]):
+        self.namespace = namespace
+        self.name = name
+        self.functions_core = functions[0]
+        self.functions_video = functions[1]
+
+
+def params2pyi(params: str) -> List[str]:
+    params = params.strip(";")
     if params == "":
         return params, None
     list_type_regex = re.compile(r"^(?P<type>\S+)\[\]$")
@@ -40,60 +53,56 @@ def convert2pyi(params: str):
         list_type_match = list_type_regex.match(param_name_type[1])
         if list_type_match is not None:
             list_type = list_type_match.groupdict()["type"]
-            param_name_type[1] = "list[%s]" % (list_type)
+            param_name_type[1] = f"Union[{list_type},Sequence[{list_type}]]"
+        if len(param_name_type) >= 3 and param_name_type[2] == "opt":
+            param_name_type[1] = f"Optional[{param_name_type[1]}]"
+        param_name_type[1] = param_name_type[1].replace("data", "Union[str, bytes, bytearray]").replace("clip", "VideoNode")
         param_list[param_i] = ":".join(param_name_type[0:2])
     if vn:
-        return ",".join(param_list), ",".join(param_list[1::])
+        return ",".join(param_list), ",".join(param_list[1::]).replace("VideoNode", '"VideoNode"')
     else:
         return ",".join(param_list), None
 
 
-for plugin_identifier in plugins:
-    plugin_dict = plugins[plugin_identifier]
-    plugin_namespace = plugin_dict["namespace"]
-    plugin_file = open("%s/%s.pyi" % (vs_path, plugin_namespace), "w+")
-    plugin_vn_file = open("%s/%s_vn.pyi" % (vs_path, plugin_namespace), "w+")
-    plugin_file_head = (
-        "from . import VideoNode as clip\nfunc = function\ndata = str\n\n"
-    )
-    plugin_file.write(plugin_file_head)
-    plugin_vn_file.write(plugin_file_head)
+def functions2pyi(functions: FunctionMeta) -> List[FunctionMeta]:
+    functions_core = functions.copy()
+    functions_video = functions.copy()
+    for name in functions.keys():
+        params = functions[name]
+        functions_core[name], functions_video[name] = params2pyi(params)
+    return functions_core, functions_video
 
-    functions = plugin_dict["functions"]
-    for function_name in functions:
-        function_params, function_vn_params = convert2pyi(
-            functions[function_name].strip(" ;")
-        )
-        function_str = "def %s(%s) -> clip: ...\n" % (function_name, function_params)
-        plugin_file.write(function_str)
-        if function_vn_params is not None:
-            function_vn_str = "def %s(%s) -> clip: ...\n" % (
-                function_name,
-                function_vn_params,
-            )
-            plugin_vn_file.write(function_vn_str)
-    plugin_file.close()
-    plugin_vn_file.close()
 
-init_in_file = open("%s" % (init_in_path), "r")
-init_file = open("%s/__init__.pyi" % (vs_path), "w+")
-init_lines = init_in_file.readlines()
-init_in_file.close()
+def plugins2str(plugins: List[PluginMeta], video: bool) -> str:
+    ret = []
+    if not video:
+        for plugin in plugins:
+            ret.append(f"    class {plugin.namespace}(Plugin):")
+            for func in plugin.functions_core.keys():
+                ret.append(f"        def {func}({plugin.functions_core[func]})->VideoNode:...")
+    else:
+        for plugin in plugins:
+            if len(plugin.functions_video) != 0:
+                ret.append(f"    class {plugin.namespace}(Plugin):")
+                for func in plugin.functions_video.keys():
+                    ret.append(f'        def {func}({plugin.functions_video[func]})->"VideoNode":...')
+    return "\n".join(ret)
 
-init_lines.append("\n")
-init_lines.append("class Core(_Core):\n")
-for plugin_identifier in plugins:
-    plugin_dict = plugins[plugin_identifier]
-    plugin_namespace = plugin_dict["namespace"]
-    init_lines.append("    from . import %s\n" % (plugin_namespace))
 
-init_lines.append("\n")
-init_lines.append("class VideoNode(_VideoNode):\n")
-for plugin_identifier in plugins:
-    plugin_dict = plugins[plugin_identifier]
-    plugin_namespace = plugin_dict["namespace"]
-    init_lines.append(
-        "    from . import %s_vn as %s\n" % (plugin_namespace, plugin_namespace)
-    )
+plugins = core.get_plugins()
+plugins_meta = []
 
-init_file.writelines(init_lines)
+for plugin in plugins.values():
+    plugin_meta = PluginMeta(plugin["namespace"], plugin["name"], functions2pyi(plugin["functions"]))
+    plugin_meta.functions_video = {key: val for key, val in plugin_meta.functions_video.items() if val is not None}
+    plugins_meta.append(plugin_meta)
+
+pyi_in_file = open(f"{init_in_path}", "r")
+pyi_content = pyi_in_file.read()
+pyi_in_file.close()
+
+pyi_content = pyi_content.replace(r"# inject Core plugins", plugins2str(plugins_meta, False))
+pyi_content = pyi_content.replace(r"# inject VideoNode plugins", plugins2str(plugins_meta, True))
+
+pyi_file = open(f"{vs_path}", "w+")
+pyi_file.write(pyi_content)
