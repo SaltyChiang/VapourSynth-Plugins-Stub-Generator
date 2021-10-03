516dae85ad073307622fab9548ed80401ef6dce9
SFNozomi <35213529+SaltyChiang@users.noreply.github.com>
2021-10-04 02:12:58 +0800

    Make it compatible with VapourSynth R56 or higher.

--- vsstubs/vsstubs/convert.py
+++ vsstubs/vsstubs/convert.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+from typing import Dict, Generator, List, Union
 
 
 class FunctionMeta(Dict[str, Union[str, None]]):
@@ -10,59 +10,72 @@ class PluginMeta:
     name: str
     functions_core: FunctionMeta
     functions_video: FunctionMeta
+    functions_audio: FunctionMeta
 
     def __init__(self, namespace: str, name: str, functions: List[FunctionMeta]):
         self.namespace = namespace
         self.name = name
         self.functions_core = functions[0]
         self.functions_video = functions[1]
+        self.functions_audio = functions[2]
 
 
 def function_params_vs2py(params: str) -> List[str]:
-    """ Type information from https://github.com/vapoursynth/vapoursynth/blob/master/src/cython/vapoursynth.pyx#L385 """
+    """ Type information from https://github.com/vapoursynth/vapoursynth/blob/master/src/cython/vapoursynth.pyx#L449 """
     params = params.strip(";")
     if params == "":
         return params, None
     param_list = params.split(";")
 
-    video = param_list[0].split(":")[1] in ["clip", "clip[]"]
+    video = param_list[0].split(":")[1] in ["vnode", "vnode[]"]
+    audio = param_list[0].split(":")[1] in ["anode", "anode[]"]
 
     for param_i in range(len(param_list)):
         name_type_opt = param_list[param_i].split(":")
-        pname = name_type_opt[0]
-        ptype = name_type_opt[1]
-        parray = ptype.endswith("[]")
-        popt = (len(name_type_opt) >= 3) and (name_type_opt[2] == "opt")
+        if len(name_type_opt) == 1 and name_type_opt[0] == "any":
+            pname = "**kwargs"
+            ptype = "Any"
+        else:
+            pname = name_type_opt[0]
+            ptype = name_type_opt[1]
+            parray = ptype.endswith("[]")
+            popt = (len(name_type_opt) >= 3) and (name_type_opt[2] == "opt")
 
-        ptype = ptype[:-2] if parray else ptype
-        ptype = ptype.replace("clip", "VideoNode").replace("frame", "VideoFrame")
-        ptype = ptype.replace("func", "Callable").replace("data", "Union[str, bytes, bytearray]")
-        ptype = f"Union[{ptype},Sequence[{ptype}]]" if parray else ptype
-        # ptype = f"Optional[{ptype}]=..." if popt else ptype
-        ptype = f"{ptype}=..." if popt else ptype
+            ptype = ptype[:-2] if parray else ptype
+            ptype = ptype.replace("vnode", "VideoNode").replace("vframe", "VideoFrame")
+            ptype = ptype.replace("anode", "AudioNode").replace("aframe", "AudioFrame")
+            ptype = ptype.replace("func", "Callable").replace("data", "Union[str, bytes, bytearray]")
+            ptype = f"Union[{ptype},Sequence[{ptype}]]" if parray else ptype
+            # ptype = f"Optional[{ptype}]=..." if popt else ptype
+            ptype = f"{ptype}=..." if popt else ptype
         param_list[param_i] = ":".join([pname, ptype])
 
     if video:
-        return ",".join(param_list), ",".join(param_list[1::]).replace("VideoNode", '"VideoNode"')
+        return ",".join(param_list), ",".join(param_list[1::]).replace("VideoNode", '"VideoNode"'), None
+    elif audio:
+        return ",".join(param_list), None, ",".join(param_list[1::]).replace("AudioNode", '"AudioNode"')
     else:
-        return ",".join(param_list), None
+        return ",".join(param_list), None, None
 
 
 def functions_vs2py(functions: FunctionMeta) -> List[FunctionMeta]:
     functions_core = functions.copy()
     functions_video = functions.copy()
+    functions_audio = functions.copy()
     for name in functions.keys():
         params = functions[name]
-        functions_core[name], functions_video[name] = function_params_vs2py(params)
-    return functions_core, functions_video
+        functions_core[name], functions_video[name], functions_audio[name] = function_params_vs2py(params)
+    return functions_core, functions_video, functions_audio
 
 
-def plugins_vs2py(plugins: Dict[str, Dict[str, Union[str, Dict[str, str]]]]) -> List[PluginMeta]:
+def plugins_vs2py(plugins: Generator) -> List[PluginMeta]:
     plugins_meta = []
 
-    for plugin in plugins.values():
-        plugin_meta = PluginMeta(plugin["namespace"], plugin["name"], functions_vs2py(plugin["functions"]))
-        plugin_meta.functions_video = {key: val for key, val in plugin_meta.functions_video.items() if val is not None}
+    for plugin in plugins:
+        functions_dict: Dict[str, str] = {}
+        for function in plugin.functions():
+            functions_dict[function.name] = function.signature
+        plugin_meta = PluginMeta(plugin.namespace, plugin.name, functions_vs2py(functions_dict))
         plugins_meta.append(plugin_meta)
 
     return plugins_meta
--- vsstubs/vsstubs/generate.py
+++ vsstubs/vsstubs/generate.py
@@ -3,38 +3,56 @@ from . import convert
 from .convert import PluginMeta
 
 
-def plugins_str(plugins: List[PluginMeta], video: bool, indent: int = 4) -> str:
-    lines = []
-    if not video:
-        for plugin in plugins:
-            lines.append(f"class {plugin.namespace}(Plugin):")
-            lines.append(f'    """{plugin.name}"""')
-            for func in plugin.functions_core.keys():
-                lines.append(f"    def {func}({plugin.functions_core[func]})->VideoNode:...")
-        lines = [" " * indent + line for line in lines]
-    else:
-        for plugin in plugins:
-            if len(plugin.functions_video) != 0:
-                lines.append(f"class {plugin.namespace}(Plugin):")
-                lines.append(f'    """{plugin.name}"""')
-                for func in plugin.functions_video.keys():
-                    lines.append(f'    def {func}({plugin.functions_video[func]})->"VideoNode":...')
-        lines = [" " * indent + line for line in lines]
-    return "\n".join(lines)
+def plugins_str(plugins: List[PluginMeta], indent: int = 4) -> str:
+    ret_core = []
+    ret_video = []
+    ret_audio = []
+    for plugin in plugins:
+        lines_core = []
+        lines_video = []
+        lines_audio = []
+        lines_core.append(f"class {plugin.namespace}(Plugin):")
+        lines_core.append(f'    """{plugin.name}"""')
+        lines_video.append(f"class {plugin.namespace}(Plugin):")
+        lines_video.append(f'    """{plugin.name}"""')
+        lines_audio.append(f"class {plugin.namespace}(Plugin):")
+        lines_audio.append(f'    """{plugin.name}"""')
+        for func in plugin.functions_core.keys():
+            if plugin.functions_video[func] is not None:
+                lines_core.append(f"    def {func}({plugin.functions_core[func]})->VideoNode:...")
+                lines_video.append(f"    def {func}({plugin.functions_video[func]})->VideoNode:...")
+            elif plugin.functions_audio[func] is not None:
+                lines_core.append(f"    def {func}({plugin.functions_core[func]})->AudioNode:...")
+                lines_audio.append(f"    def {func}({plugin.functions_audio[func]})->AudioNode:...")
+            else:
+                lines_core.append(f"    def {func}({plugin.functions_core[func]})->VideoNode:...")
+        if len(lines_core) > 2:
+            ret_core += lines_core
+        if len(lines_video) > 2:
+            ret_video += lines_video
+        if len(lines_audio) > 2:
+            ret_audio += lines_audio
+
+    ret_core = [" " * indent + line for line in ret_core]
+    ret_video = [" " * indent + line for line in ret_video]
+    ret_audio = [" " * indent + line for line in ret_audio]
+
+    return "\n".join(ret_core), "\n".join(ret_video), "\n".join(ret_audio)
 
 
 def stub() -> str:
     import os
     from vapoursynth import core
 
-    plugins = core.get_plugins()
-    plugins_meta = convert.plugins_vs2py(plugins)
+    plugins_meta = convert.plugins_vs2py(core.plugins())
 
     moduledir = os.path.dirname(os.path.realpath(__file__))
     with open(f"{moduledir}/vapoursynth.pyi.in", "r") as f:
         pyi_content = f.read()
 
-    pyi_content = pyi_content.replace(r"# inject Core plugins", plugins_str(plugins_meta, False, 4))
-    pyi_content = pyi_content.replace(r"# inject VideoNode plugins", plugins_str(plugins_meta, True, 4))
+    plugins_core, plugins_video, plugins_audio = plugins_str(plugins_meta, 4)
+    pyi_content = pyi_content.replace(r"# inject Core plugins", plugins_core)
+    pyi_content = pyi_content.replace(r"# inject VideoNode plugins", plugins_video)
+    pyi_content = pyi_content.replace(r"# inject AudioNode plugins", plugins_audio)
 
     return pyi_content
